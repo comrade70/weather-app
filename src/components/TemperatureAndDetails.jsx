bdc08bb5927d292e347261eb147da786c574330a
comrade70 <ademolajoseph88@gmail.com>
2024-09-17 23:08:08 +0100

    adding Forecast component

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { UilTemperature, UilTear, UilWind } from '@iconscout/react-unicons';
+import { UilTemperature, UilTear, UilWind, UilSun, UilSunset } from '@iconscout/react-unicons';
 
 function TemperatureAndDetails() {
   return (
@@ -14,7 +14,7 @@ function TemperatureAndDetails() {
                 <p className="text-5xl">34°</p>
 
                 <div className="flex flex-col space-y-2 ">
-                    <div className="flex font-light text-sm items-center justify-left">
+                    <div className="flex font-light text-sm items-center justify-center">
                         <UilTemperature size={18} className="mr-1"/>
                         Temperature:
                         <span className='font-medium ml-1'>32°</span>
@@ -30,7 +30,32 @@ function TemperatureAndDetails() {
                         <span className='font-medium ml-1'>10km/h</span>
                     </div>
                 </div>
-            </div>   
+            </div>  
+            <div className="flex flex-row item-center justify-center space-x-2 text-white text-sm py-3">
+                <UilSun/>
+                <p className="font-light">
+                    Rise: <span className='font-medium text-ml-1 '>6: 45 AM</span>
+                </p>   
+                <p className="font-light">|</p> 
+
+                <UilSunset/>
+                <p className="font-light">
+                    Set: <span className='font-medium text-ml-1 '>7: 35 PM</span>
+                </p>   
+                <p className="font-light">|</p> 
+
+                <UilSun/>
+                <p className="font-light">
+                    High: <span className='font-medium text-ml-1 '>45°</span>
+                </p>   
+                <p className="font-light">|</p> 
+
+                <UilSun/>
+                <p className="font-light">
+                    Low: <span className='font-medium text-ml-1 '>40°</span>
+                </p>   
+                <p className="font-light">|</p> 
+            </div> 
     </div>
   );
 }
